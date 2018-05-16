b18141867f9e11b2ed7c6b567feae56c219b0094
voiceferate <vanyavi06@gmail.com>
2018-05-16 17:49:33 +0300

    added slider

--- app/js/common.js
+++ app/js/common.js
@@ -20,12 +20,42 @@ $(function() {
     });
 
     $(document).ready(function() {
+
         var a = $('#recent-posts-3 li');
         console.log(a)
         for (var val of a) {
             console.log($(val).find('span'))
             $(val).find('a').append($(val).find('span'))
         }
+
+        $('.owl-carousel').owlCarousel({
+            smartSpeed: 1500,    
+            responsiveClass:true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            checkVisible: false,
+            responsive:{
+                0:{
+                    items: 1,
+                    nav: false,
+                    loop: true,
+
+                },
+                600:{
+                    items: 1,
+                    nav: false,
+                    loop: true
+                },
+                1000:{
+                    items: 1,
+                    nav: false,
+                    loop: true,
+                    autoplay: true
+                }
+            }
+        })
     });
+
+
 
 });
