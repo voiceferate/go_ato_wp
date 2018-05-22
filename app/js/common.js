77ba5463693bf25c9f40c2779011340472e22ba2
voiceferate <vanyavi06@gmail.com>
2018-05-22 17:28:39 +0300

    map added

--- app/js/common.js
+++ app/js/common.js
@@ -31,8 +31,10 @@ $(function() {
             $(val).find('a').append($(val).find('span'))
         }
 
-        window.scrollTo(0, ($('.slider').offset().top))
-        console.log($('.slider').offset().top)
+        if ($('.slider').length) {
+            window.scrollTo(0, ($('.slider').offset().top))
+            console.log($('.slider').offset().top)
+        }
 
         $('.owl-carousel').owlCarousel({
             smartSpeed: 1500,    
@@ -61,6 +63,20 @@ $(function() {
         })
     });
 
-
+//map api AIzaSyBN-_YHch6wA1CzF1SEL3Yv7X49DR0UvBU
 
 });
+
+function initMap() {
+    var spilka = {lat: 49.025248, lng: 24.361334};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: spilka
+    });
+    var marker = new google.maps.Marker({
+      map: map,
+      position: spilka,
+      animation: google.maps.Animation.DROP,
+      title:"Громадське об’єднання Калущини учасників бойових дій (АТО) на сході України імені Романа Шухевича"
+    });
+  }
